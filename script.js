3a4ed0de239c96e2d6aa757679f9a1dec335cb43
Renchester Ramos <renchesterjramos@gmail.com>
2021-07-20 03:04:43 +0800

    Add initial  game control modules

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+// Game module
+const game = (() => {
+  const board = ['', '', '', '', '', '', '', '', ''];
+
+  return {
+    board,
+  };
+})();
+
+const Player = (tictac) => {
+  const setTictac = () => tictac;
+  return { setTictac };
+};
+
+const player1 = Player('o');
+
+const controlGame = (() => {
+  const _squares = document.querySelectorAll('.square');
+
+  const _placeTicTac = (e) => {
+    const squareEl = e.target.closest('.square');
+
+    if (squareEl.dataset.filled === 'true') return;
+
+    game.board[squareEl.dataset.id - 1] = player1.setTictac();
+
+    controlDisplay.fillBoard();
+  };
+
+  const addSquareEvents = () => {
+    _squares.forEach((square) =>
+      square.addEventListener('click', _placeTicTac)
+    );
+  };
+
+  return { addSquareEvents };
+})();
+
+const controlDisplay = (() => {
+  const _squares = document.querySelectorAll('.square');
+
+  const fillBoard = () => {
+    _squares.forEach(
+      (square) =>
+        (square.innerHTML = `<div class="place-${
+          game.board.flat()[square.dataset.id - 1]
+        }"></div> `)
+    );
+  };
+
+  return { fillBoard };
+})();
+
+controlGame.addSquareEvents();
+controlDisplay.fillBoard();
