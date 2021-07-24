cf92c31d8a98b7fcb49af9bd096f76892853e71c
Renchester Ramos <renchesterjramos@gmail.com>
2021-07-24 08:37:49 +0800

    Add play against AI easy mode

--- script.js
+++ script.js
@@ -18,23 +18,35 @@ const Player = (tictac, name) => {
   return { getTictac, defaultName, score };
 };
 
-const player1 = Player('o', 'Player 1');
-const player2 = Player('x', 'Player 2');
+const player1 = Player('x', 'Player 1');
+const player2 = Player('o', 'Player 2');
+const ai = Player('o', 'The Computer');
 
 // Game initializer module
 
+let opponent;
+let diffLevel;
+
 const initializeGame = (() => {
-  let opponent;
   const _opponents = document.querySelectorAll('.opponent');
-  const _opponentPlayer = document.querySelector('.opponent.opp-player');
-  const _opponentAI = document.querySelector('.opponent.opp-ai');
   const _nameInputPlayer1 = document.querySelector('.player-1.name-input');
   const _nameInputPlayer2 = document.querySelector('.player-2.name-input');
   const _btnStartGame = document.querySelector('.start-game');
+  const _diffLevels = document.querySelectorAll('.diff-level');
 
   function _chooseOpponent(e) {
     opponent = e.target.closest('.opponent').dataset.opponent;
-    controlDisplay.showPlayers();
+    opponent === 'player'
+      ? controlDisplay.showPlayers()
+      : controlDisplay.showDifficulty();
+
+    controlDisplay.changeNamesForAI();
+  }
+
+  function _chooseDiffLevel(e) {
+    diffLevel = e.target.closest('.diff-level').dataset.difficulty;
+
+    controlDisplay.showGame();
   }
 
   function _startGame(e) {
@@ -49,9 +61,11 @@ const initializeGame = (() => {
 
   _opponents.forEach((el) => el.addEventListener('click', _chooseOpponent));
 
+  _diffLevels.forEach((el) => el.addEventListener('click', _chooseDiffLevel));
+
   _btnStartGame.addEventListener('click', _startGame);
 
-  return { opponent };
+  return { opponent, diffLevel };
 })();
 
 // Game controller module
@@ -62,7 +76,11 @@ const controlGame = (() => {
   let result;
 
   function _switchPlayer(player = player2) {
-    return player === player1 ? player2 : player1;
+    if (opponent === 'player') {
+      return player === player1 ? player2 : player1;
+    } else if (opponent === 'ai') {
+      return player === player1 ? ai : player1;
+    } else if (!opponent) return player1;
   }
 
   function _placeTicTac(e) {
@@ -80,8 +98,15 @@ const controlGame = (() => {
 
     if (result) return;
 
-    controlDisplay.displayActive(currPlayer);
-    currPlayer = _switchPlayer(currPlayer);
+    if (opponent === 'player') {
+      controlDisplay.displayActive(currPlayer);
+      currPlayer = _switchPlayer(currPlayer);
+    }
+
+    if (opponent === 'ai' && diffLevel === 'easy') {
+      controlDisplay.displayActive(player2);
+      _generateEasyPlay();
+    }
   }
 
   function _checkWin() {
@@ -105,8 +130,18 @@ const controlGame = (() => {
         game.board[winningIndices[i][0]] === game.board[winningIndices[i][2]]
       ) {
         result = 'win';
-        currPlayer.score++;
-        controlDisplay.displayWinner(currPlayer);
+
+        if (opponent === 'player') {
+          currPlayer.score++;
+          controlDisplay.displayWinner(currPlayer);
+        }
+
+        if (opponent === 'ai') {
+          const winnerVsAI =
+            game.board[winningIndices[i][0]] === 'x' ? player1 : ai;
+          winnerVsAI.score++;
+          controlDisplay.displayWinner(winnerVsAI);
+        }
 
         const _btnPlayAgain = document.querySelector('.btn.play-again');
         _btnPlayAgain.addEventListener('click', playAgain);
@@ -143,7 +178,26 @@ const controlGame = (() => {
     controlDisplay.resetGameDisplay();
   }
 
-  return { addSquareEvents, currPlayer, result };
+  function _generateEasyPlay() {
+    const randomInput = Math.floor(Math.random() * 9);
+    const boardArr = Array.from(_squares);
+
+    if (!game.board[randomInput]) {
+      game.board[randomInput] = 'o';
+      const squareToFill = boardArr.find(
+        (square) => +square.dataset.id === +randomInput
+      );
+      squareToFill.dataset.filled = 'true';
+    } else if (game.board.every((el) => el)) {
+      _checkDraw();
+    } else if (game.board[randomInput]) {
+      _generateEasyPlay();
+    }
+
+    controlDisplay.fillBoard();
+  }
+
+  return { addSquareEvents, currPlayer, result, playAgain };
 })();
 
 // Display controller module
@@ -152,6 +206,7 @@ const controlDisplay = (() => {
   const _squares = document.querySelectorAll('.square');
   const _gameOpponents = document.querySelector('.game-opponents');
   const _gamePlayers = document.querySelector('.game-players');
+  const _gameAI = document.querySelector('.game-ai');
   const _gameContainer = document.querySelector('.game-container');
   const _namePlayer1 = document.querySelector('.player-1.name');
   const _namePlayer2 = document.querySelector('.player-2.name');
@@ -170,7 +225,13 @@ const controlDisplay = (() => {
     _gamePlayers.classList.remove('hidden');
   }
 
+  function showDifficulty() {
+    _gameOpponents.classList.add('hidden');
+    _gameAI.classList.remove('hidden');
+  }
+
   function showGame() {
+    _gameAI.classList.add('hidden');
     _gamePlayers.classList.add('hidden');
     _gameContainer.classList.remove('hidden');
   }
@@ -186,6 +247,11 @@ const controlDisplay = (() => {
   function displayNames() {
     _namePlayer1.textContent = player1.name;
     _namePlayer2.textContent = player2.name;
+  }
+
+  function changeNamesForAI() {
+    _namePlayer1.textContent = 'YOU';
+    _namePlayer2.textContent = 'AI';
   }
 
   function displayActive(currPlayer) {
@@ -206,7 +272,8 @@ const controlDisplay = (() => {
     _btnPlayAgain.classList.remove('hidden');
 
     _scorePlayer1.textContent = player1.score;
-    _scorePlayer2.textContent = player2.score;
+    _scorePlayer2.textContent =
+      opponent === 'player' ? player2.score : ai.score;
   }
 
   function displayDraw() {
@@ -227,7 +294,9 @@ const controlDisplay = (() => {
   return {
     fillBoard,
     showPlayers,
+    showDifficulty,
     showGame,
+    changeNamesForAI,
     displayActive,
     displayWinner,
     displayDraw,
